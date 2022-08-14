df9ad9b6cfe7d5c207e1692261c9b11236c36411
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-08-14 11:09:14 -0500

    Adds additional layouts and routes for stories page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 
 //Load config
@@ -37,6 +38,9 @@ app.use(
         secret:'keyboard cat',
         resave: false,
         saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGO_URI
+        })
     })
 )
 
@@ -51,7 +55,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 //Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
-
+app.use('/stories', require('./routes/stories'))
 const PORT = process.env.PORT || 8500
 
 app.listen(PORT, console.log(`Server running on ${process.env.NODE_ENV} mode on PORT ${PORT}`))
